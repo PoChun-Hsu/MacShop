6c7f0083c2a37160c0ac3e148f31b0a992318bd8
PoChun-Hsu <150916046+PoChun-Hsu@users.noreply.github.com>
2026-04-07 20:15:40 +0800

    Add init_db.py to create Trino database
    Initialize Trino database in Superset if it doesn't exist.

---
+++ airflow3-docker/superset/init_db.py
@@ -0,0 +1,24 @@
+from superset import db
+from superset.models.core import Database
+
+db_uri = "trino://trino@trino:8080/iceberg"
+
+database_name = "Trino"
+
+existing = db.session.query(Database).filter_by(database_name=database_name).first()
+
+if not existing:
+    database = Database(
+        database_name=database_name,
+        sqlalchemy_uri=db_uri,
+        expose_in_sqllab=True,
+        allow_run_async=True,
+    )
+
+    database.allow_dml = True
+
+    db.session.add(database)
+    db.session.commit()
+    print("✅ Database created")
+else:
+    print("⚠️ Database already exists")
