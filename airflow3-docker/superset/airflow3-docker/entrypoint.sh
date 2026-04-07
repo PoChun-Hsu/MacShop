81469cd6348d3e7cfc1cde5b5d23b56cb346a486
PoChun-Hsu <150916046+PoChun-Hsu@users.noreply.github.com>
2026-04-07 20:14:49 +0800

    Create entrypoint.sh for Superset setup
    Add entrypoint script for initializing and running Superset.

---
+++ airflow3-docker/superset/airflow3-docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "🔧 Superset init start"
+
+superset db upgrade
+
+superset fab create-admin \
+  --username admin \
+  --firstname admin \
+  --lastname admin \
+  --email admin@admin.com \
+  --password admin || true
+
+superset init
+
+echo "🔧 Create database connection"
+superset shell < /app/init_db.py
+
+echo "🚀 Start Superset"
+superset run -h 0.0.0.0 -p 8088
