f38589522e30395c5406f32a3230763c60fe070f
PoChun-Hsu <150916046+PoChun-Hsu@users.noreply.github.com>
2026-04-08 20:04:38 +0800

    Update init_db.py

--- airflow3-docker/superset/init_db.py
+++ airflow3-docker/superset/init_db.py
@@ -1,7 +1,8 @@
-# 20260407_001 - PoChun Hsu - [Add]     Create database and dataset for superset.
-
 from superset import db
 from superset.models.core import Database
+from superset.connectors.sqla.models import SqlaTable
+
+print("🔥 INIT DB START")
 
 db_uri = "trino://trino@trino:8080/iceberg"
 
@@ -14,7 +15,7 @@ if not existing:
         database_name=database_name,
         sqlalchemy_uri=db_uri,
         expose_in_sqllab=True,
-        allow_run_async=True,
+        allow_run_async=False,
     )
 
     database.allow_dml = True
@@ -22,5 +23,23 @@ if not existing:
     db.session.add(database)
     db.session.commit()
     print("✅ Database created")
+
+    table_name = "test"
+    schema = "default"
+
+    dataset = db.session.query(SqlaTable).filter_by(
+        table_name=table_name,
+        schema=schema
+    ).first()
+
+    if not dataset:
+        dataset = SqlaTable(
+            table_name=table_name,
+            schema=schema,
+            database=database,
+        )
+        db.session.add(dataset)
+        db.session.commit()
+        print("✅ Dataset created")
 else:
     print("⚠️ Database already exists")
